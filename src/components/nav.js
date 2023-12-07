2c792c4c3c491354598d9cb6c41b5de2a2d4b8ce
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-12-07 11:03:10 +0800

    Add files via upload

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,5 @@
 import React, {useState,useEffect} from 'react'
+import ReactFlagsSelect from "react-flags-select";
 // css
 import './styles/nav.css'
 import './styles/create-modal.css'
@@ -12,13 +13,25 @@ const Nav = () => {
 
   // useState
 const [ navDown, setNavdown ] = useState(false);
-const [ create,setCreate ] = useState(false)
+const [ create,setCreate ] = useState(false);
+const [ signup,setSignUp ] = useState(false);
+const [selected, setSelected] = useState("");
 
+// nav login btn
 const createAccount = () => {
     setCreate(true)
 }
 const closeCreate = () => {
     setCreate(false)
+}
+
+// create account btn
+const signupAccount = () => {
+  setSignUp(true)
+}
+// login account
+const loginAcc = () => {
+  setSignUp(false)
 }
 // useEffect
 useEffect(() => {
@@ -43,24 +56,59 @@ useEffect(() => {
                 <div className="motto-modal">
                     <p>task done, pay fast</p>
                 </div>
-                <div className="use-google-btn">
-                    <button><FcGoogle id='google-icon'/>Continue using Google Account</button>
-                </div>
-                <p>Login in Tasktify Account</p>
-                <div className="input-login">
-                <input type="text" placeholder='Username'/>
-                <input type="text" placeholder='Password'/>
-                <button>Login</button>
-                </div>
-                <p>Don't have Tasktify Account?</p>
-                <li>Create Account Now</li>
-                <div className="create-foot">
-                <p>Connect with us at:</p>
-                <div className="foot-icons">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png" alt="" />
-                    <img src="https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png" alt="" />
-                </div>
-                </div>
+                <section className={`login-account ${signup}`}>
+                  <div className="use-google-btn">
+                      <button><FcGoogle id='google-icon'/>Continue using Google Account</button>
+                  </div>
+                  <p>Login in Tasktify Account</p>
+                  <div className="input-login">
+                  <input type="text" placeholder='Username'/>
+                  <input type="text" placeholder='Password'/>
+                  <button>Login</button>
+                  </div>
+                  <p>Don't have Tasktify Account?</p>
+                  <li onClick={signupAccount}>Create Account Now</li>
+                  <div className="create-foot">
+                  <p>Connect with us at:</p>
+                  <div className="foot-icons">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png" alt="" />
+                      <img src="https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png" alt="" />
+                  </div>
+                  </div>
+                </section>
+                <section className={`create-account-now ${signup}`}>
+                  <div className="signup">
+                    <form action="">
+                      <input type="text" placeholder='Email'/>
+                      <div className="fill-name">
+                        <input type="text" placeholder='First Name'/>
+                        <input type="text" placeholder='Last Name'/>
+                      </div>
+                      <ReactFlagsSelect
+                        selected={selected}
+                        onSelect={(code) => setSelected(code)}
+                        className='countries'
+                        placeholder="Select Country"
+                        searchable
+                        selectedSize={10}
+                        optionsSize={10}
+                      />
+                      <input type="integer" placeholder='Mobile no.' maxLength="11"/>
+                      <div className="agree-create">
+                        <input type="checkbox" name="" id="" />
+                        <p>I understand and accept the <span>Terms and Conditions of Tasktify</span></p>
+                      </div>
+                      <button>Create Account</button>
+                    </form>
+                  </div>
+                  <p>Already have an Account?</p>
+                  <li onClick={loginAcc}>Login Account</li>
+                  <p>Connect with us at:</p>
+                  <div className="foot-icons">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png" alt="" />
+                      <img src="https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png" alt="" />
+                  </div>
+                </section>
             </div>
         </div>
         <div className="close-outside" onClick={closeCreate}></div>
@@ -73,9 +121,9 @@ useEffect(() => {
           </div>
           <div className="navlist">
             <ul>
-              <li>Projects</li>
-              <li>Designs</li>
-              <li>Contacts</li>
+              <li>Explore</li>
+              <li>Locations</li>
+              <li>Services</li>
               <button onClick={createAccount}>Login / Signup</button>
             </ul>
           </div>
